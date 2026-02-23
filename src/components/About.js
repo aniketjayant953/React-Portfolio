14a9b516bba801a96107ac9c975c2b3822bf588a
aniketjayant953 <aniketjayant953@gmail.com>
2026-02-24 00:18:14 +0530

    Updated Project files

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,5 @@
 import aboutImg from "../assets/img/self.jpg";
-import expImg from "../assets/img/experience.png";
+import expImg from "../assets/img/suitcase.png";
 import eduImg from "../assets/img/education.png";
 
 export const About = () => {
@@ -20,21 +20,21 @@ export const About = () => {
             
           <div class="details-container">
             <img src={expImg} alt="Experience icon" class="icon"/>
-            <h3>Data Science Course</h3>
-            <p>1+ years <br/>Data Scientist/Data Analyst</p>
+            <h3>Data Analyst</h3>
+            <p>3.5+ years <br/>at YOUNGELEMENT INDIA PRIVATE LIMITED PVT LTD </p>
           </div>
 
           <div class="details-container">
             <img src={eduImg} alt="Education icon"class="icon" />   
             <h3>Education</h3>
-            <p>M.Engg Bachelors Degree<br/>M.Engg Diploma </p>
+            <p>Bachelors in Engineering Degree<br/> SPPU University </p>
           </div>
 
         </div>
 
         <div class="text-container">
           <p>
-          Hi, my name is Aniket and I am a Data Science/Analyst Aspirant. I hold a Bachelor's Degree in Mechanical Engineering and Course certification in Data Science/Analytics.
+          Hi, my name is Aniket and I am a Data Science/Analyst Aspirant. I hold a Bachelor's Degree from SPPU University and Course certification in Data Science/Analytics.
 
 Since I was young, I have always enjoyed to solve puzzles. So that's how I look at big data sets, to me it is one big puzzle I want to solve. Finding patterns nobody else sees is the challenge to me. Before I started my journey in Data domain I had studied for a year as a Data Analyst/Scientist in Project Execution and Processing. Do you want to work together? Please reach out to me by e-mail.
           </p>
